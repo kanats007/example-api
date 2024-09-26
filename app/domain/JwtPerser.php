3635263c14852e8ec7e8d00791dc081df80b074c
kanats <kanats007m@gmail.com>
2024-09-26 10:05:04 +0900

    JWT解析クラス追加

---
+++ app/domain/JwtPerser.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\domain;
+
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Lcobucci\JWT\Token\Parser;
+use Lcobucci\JWT\UnencryptedToken;
+
+class JwtPerser {
+
+    public static function parse(string $token): UnencryptedToken
+    {
+        $parser = new Parser(new JoseEncoder());
+        return $parser->parse($token);
+    }
+}
+
