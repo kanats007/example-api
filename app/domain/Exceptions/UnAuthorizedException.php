5851479be9fc3900835e9044e0c5763071d60172
kanats <kanats007m@gmail.com>
2024-09-28 19:48:36 +0900

    例外クラスの追加

---
+++ app/domain/Exceptions/UnAuthorizedException.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\domain\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UnAuthorizedException extends Exception
+{
+
+    public function __construct(string $message = '')
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 例外をレポート
+     */
+    public function report(): void
+    {
+        // ...
+    }
+
+    /**
+     * 例外をHTTPレスポンスへレンダ
+     * @see https://readouble.com/laravel/11.x/ja/errors.html#renderable-exceptions
+     */
+    public function render(Request $request): JsonResponse
+    {
+        return response()->json(['message' => 'unauthorized.'], Response::HTTP_UNAUTHORIZED, [], JSON_UNESCAPED_UNICODE);
+    }
+}
